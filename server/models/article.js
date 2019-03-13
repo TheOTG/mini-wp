f12205b7f88729c70566ed6d3a6a7fccf38bafcb
TheOTG <kennythesman@gmail.com>
2019-03-13 12:28:15 +0700

    remove pre save hook and added default for created_at

--- server/models/article.js
+++ server/models/article.js
@@ -4,12 +4,10 @@ const Schema = mongoose.Schema
 let articleSchema = new Schema({
     title: String,
     content: String,
-    created_at: Date
-})
-
-articleSchema.pre('save', function (next) {
-    this.created_at = new Date(this.created_at)
-    next()
+    created_at: {
+        type: Date,
+        default: new Date()
+    }
 })
 
 const Article = mongoose.model('Articles', articleSchema)
